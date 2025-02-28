162cec6f22ec3a88b91a5fd189cb146daf25e4d4
TeroddeFB <franciscomozzonimartin@gmail.com>
2025-02-28 07:48:58 -0300

    Séptimo Commit

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,10 +1,12 @@
 function evitarEnvio(event) {
     event.preventDefault(); // Evita el envío del formulario
-    alert("Formulario enviado (en realidad no)"); // Ejemplo: Muestra una alerta
+    alert("Form submitted"); // Ejemplo: Muestra una alerta
     const formulario = document.getElementById("miFormulario");
     formulario.reset();
     // Aquí puedes agregar tu lógica de validación o cualquier otra acción
   }
+
+
 
 let elementosOtr = [];
 
@@ -23,7 +25,13 @@ function performSearch() {
           resultados = data.beaches;
         } else if (inputNue === "temple" || inputNue === "temples") {
           resultados = data.temples;
+        } else if (inputNue === "country" || inputNue === "countries") {
+          resultados = data.countries;
         } 
+        else if (inputNue === ""){
+          resultados = "";
+          alert("Fill it!");
+        }
         else {
           const pais = data.countries.find((country) =>
             country.name.toLowerCase() === inputNue
@@ -32,24 +40,34 @@ function performSearch() {
             resultados = pais.cities;
           }
           else {
-            resultados = "No destinations yet"
-            alert(resultados)
+            resultados = "";
+            alert("No destinations")
           }
         }
+        if (resultados != ""){
         console.log(resultados);
         const outputDiv = document.getElementById("outputDiv");
         const destinations = resultados.map(destination => `<div class="cajita">
             <p class="grande"><strong>${destination.name}</strong></p>
             <p class="chico">${destination.description}</p>
+            <p class="chico">${horas(destination.time)}</p>
             <img src=${destination.imageUrl} class="foto"></img>
             </div>`);
+
+            function horas(horario){
+                const options = { timeZone: horario, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+                const newTime = new Date().toLocaleTimeString('en-US', options);
+                return "Current time: " + newTime;
+            }
+
+
             outputDiv.style.display = "block";
             outputDiv.innerHTML = destinations.join('');
             outputDiv.style.width = input.offsetWidth + 60 + "px"; // Ajusta el ancho
             outputDiv.style.position = "fixed";
             outputDiv.style.left = input.offsetLeft - 35 + "px";
             outputDiv.style.top = (input.offsetTop + input.offsetHeight + 10) + "px"; // Justo debajo
-            console.log(destinations);
+            console.log(destinations);}
       })
       .catch((error) => {
         console.error("Error al cargar el JSON");
@@ -69,3 +87,4 @@ function resetResults() {
     const outputDiv = document.getElementById("outputDiv");
     outputDiv.style.display = "none";
 }
+
